4614da4275862a79c33f6e1655d61a134dcfd944
Oskari Oksanen <126570043+oskarioksanen@users.noreply.github.com>
2023-10-10 12:56:29 +0300

    Publisher for user keyboard input created

---
+++ ex3/keyboard_input_pub.cpp
@@ -0,0 +1,52 @@
+#include "ros/ros.h"
+#include <std_msgs/String.h>
+
+
+/**
+ * This tutorial demonstrates simple sending of messages over the ROS system.
+ */
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "keyboard_input_pub");
+  ros::NodeHandle n;
+  ros::Publisher trajectory_publisher = n.advertise<std_msgs::String>("keyboard_input_topic", 1000);
+
+  //ros::Rate loop_rate(10);
+  ros::Rate loop_rate(0.1);
+  
+  int count = 0;
+
+  while (ros::ok())
+  {
+  
+    std_msgs::String keyboard_input;
+    std::string input;
+    
+    std::cout << "Enter \"opposite direction\" to change the robot trajectory (Other input changes trajectory back to original)" << std::endl;
+    std::getline(std::cin, input);
+    
+    for (char &c : input)
+    {
+    	c = std::tolower(c);
+    }
+    
+    if (input == "opposite direction")
+    {
+    	keyboard_input.data = "opposite";
+    }
+    else
+    {
+    	keyboard_input.data = "original";
+    }
+
+    ROS_INFO("Publisher publishing!");
+    trajectory_publisher.publish(keyboard_input);
+    
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
